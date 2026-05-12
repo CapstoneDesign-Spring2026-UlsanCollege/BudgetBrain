21f6649aca301aab63bf51ad3e027213487a8e72
bidhan pokhrel <163018924+biidhu@users.noreply.github.com>
2026-05-12 17:50:04 +0900

    Delete backend directory

--- backend/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { protect } = require("../middleware/auth");
-const { getDashboard } = require("../controllers/dashboardController");
-
-router.use(protect);
-router.get("/", getDashboard);
-
-module.exports = router;
